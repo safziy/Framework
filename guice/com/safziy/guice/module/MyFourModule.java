af562019fa2215bee6e126cebd5b0c4b82cea017
g <safziy@foxmail.com>
2016-03-18 09:41:05 +0800

    Commit

--- guice/com/safziy/guice/module/MyFourModule.java
+++ guice/com/safziy/guice/module/MyFourModule.java
@@ -8,6 +8,9 @@ public class MyFourModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		System.out.println("MyFourModule configure()");
+
+		// 单例和非单例
+		// bind(MyThirdService.class).in(Scopes.SINGLETON);
 		bind(MyThirdService.class).in(Scopes.NO_SCOPE);
 	}
 
